0916d2168cae85c5df78afb00e4e599f5089a773
Mayank Sheladiya <89696441+Mayank5054@users.noreply.github.com>
2024-03-15 10:56:36 +0000

    image crousel implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ImageCrousel } from 'src/ImageCrousel/imageCrousel.component';
 import { Navbar } from 'src/navigation/navbar.component';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  imports: [Navbar],
+  imports: [Navbar, ImageCrousel],
   standalone: true,
   styleUrls: ['./app.component.css'],
-  template: `<navbar></navbar>`,
+  template: `
+    <navbar></navbar>
+    <imageCrosuel> </imageCrosuel>
+  `,
 })
 export class AppComponent {
   title = 'CodeSandbox';
