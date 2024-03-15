5f444c7925ed50df844bc42c1e094ad071fb4951
Mayank Sheladiya <89696441+Mayank5054@users.noreply.github.com>
2024-03-15 08:31:56 +0000

    navbar created successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Navbar } from 'src/navigation/navbar.component';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  imports: [Navbar],
+  standalone: true,
   styleUrls: ['./app.component.css'],
+  template: `<navbar></navbar>`,
 })
 export class AppComponent {
   title = 'CodeSandbox';
---
+++ src/navigation/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  styleUrls: ['./navbar.css'],
+  templateUrl: './navbar.html',
+  standalone: true,
+})
+export class Navbar {}
