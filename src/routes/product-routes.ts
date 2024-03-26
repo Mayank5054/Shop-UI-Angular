2ff831568fec1f92090c28b2e250d7b8c379e264
Mayank5054 <mayanksheladiya49@gmail.com>
2024-03-26 17:35:56 +0530

    playing with routes

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from "@angular/router";
+import { ProductComponent } from "src/product/product.component";
 
 export const productRoutes : Routes = [
     {
+        path:"product/:id/:authToken",
+        component:ProductComponent
+    },
+    {
+        path:"product/:id",
+        component:ProductComponent
+    },
+    {
         path:"product",
         redirectTo:"/store"
-    }
+    },
+   
 ]
